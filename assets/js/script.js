d003596c11f6fd345bd49c5ab2d92de89e22827f
Kaella Sales <Saleskaella01@gmail.com>
2024-06-05 21:13:20 -0300

    mudança no script de como o slider se comporta(esqueci de trocar para branch carousel2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 let count = 1;
 document.getElementById("radio1").checked = true;
-
+const slideshow = document.getElementById('slider');
 
 
 setInterval(function(){
@@ -11,8 +11,26 @@ nextImage();
 function nextImage(){
     count++;
     if(count>4){
+       
+        slideshow.style.transition = 'none';
+        slideshow.style.transform = `translateX(${0})`;
         count = 1;
-    }
-    document.getElementById("radio" + count).checked = true;
+        
+
+        setTimeout(() => {
+                slideshow.style.transition = '.6s ease';
+                slideshow.style.transform = `translateX(-${ count * 20}%)`
+        }, 50);
+        
+        
+    
+   
+    
+}else{
+     slideshow.style.transition = '.6s ease';
+     slideshow.style.transform = `translateX(-${ count * 20}%)`;
+
+}
+document.getElementById("radio" + count).checked = true;
     
 }
